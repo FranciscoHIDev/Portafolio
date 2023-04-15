2d84819c3825ce53961de4b2405a8622a20f925f
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-04-14 21:45:36 -0500

    actualzacion navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -5,11 +5,11 @@ import { CgMenuGridR, CgClose } from "react-icons/cg";
 function NavBar() {
   const [openMenu, setOpenMenu] = useState(false);
   return (
-    <div className=" flex  w-full fixed bg-background md:shadow-md">
-      <div className="container mx-auto m-2 ">
+    <div className=" flex fixed w-full bg-primary md:shadow-md">
+      <div className="container sm:mx-auto md:mx-auto m-2 ">
         <div
           onClick={() => setOpenMenu(!openMenu)}
-          className="sm:flex absolute right-5 top-3 cursor-pointer md:hidden"
+          className="sm:flex absolute right-5 top-3 cursor-pointer md:hidden "
         >
           {openMenu ? (
             <CgClose className="text-white text-3xl" />
@@ -24,57 +24,56 @@ function NavBar() {
           >
             {"<FranciscoDev/>"}
           </Link>
-          <div
+
+          <ul
             className={`  md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 text-white sm:self-center text-xl border-t sm:border-none ${
               !openMenu ? "top-10 hidden" : "top-14"
             }`}
           >
-            <ul>
-              <li className="sm:inline-block ">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="home" className="p-3">
-                    Inicio
-                  </Link>
-                </button>
-              </li>
-              <li className="sm:inline-block">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="about" className="p-3">
-                    Sobre mí
-                  </Link>
-                </button>
-              </li>
-              <li className="sm:inline-block">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="skills" className="p-3">
-                    Habilidades
-                  </Link>
-                </button>
-              </li>
-              <li className="sm:inline-block">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="projects" className="p-3">
-                    Proyectos
-                  </Link>
-                </button>
-              </li>
+            <li className="sm:inline-block ">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="home" className="p-3">
+                  Inicio
+                </Link>
+              </button>
+            </li>
+            <li className="sm:inline-block">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="about" className="p-3">
+                  Sobre mí
+                </Link>
+              </button>
+            </li>
+            <li className="sm:inline-block">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="skills" className="p-3">
+                  Habilidades
+                </Link>
+              </button>
+            </li>
+            <li className="sm:inline-block">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="projects" className="p-3">
+                  Proyectos
+                </Link>
+              </button>
+            </li>
 
-              <li className="sm:inline-block">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="testimonials" className="p-3">
-                    Testimonios
-                  </Link>
-                </button>
-              </li>
-              <li className="sm:inline-block">
-                <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
-                  <Link to="contact" className="p-3">
-                    Contacto
-                  </Link>
-                </button>
-              </li>
-            </ul>
-          </div>
+            <li className="sm:inline-block">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="testimonials" className="p-3">
+                  Testimonios
+                </Link>
+              </button>
+            </li>
+            <li className="sm:inline-block">
+              <button className=" focus:bg-primary rounded-lg hover:bg-primary p-[5px]">
+                <Link to="contact" className="p-3">
+                  Contacto
+                </Link>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
