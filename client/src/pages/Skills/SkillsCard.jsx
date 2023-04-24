98862dfd10952f2a2e3860d075f77664ce476c90
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-04-24 00:20:04 -0500

    css

--- client/src/pages/Skills/SkillsCard.jsx
+++ client/src/pages/Skills/SkillsCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SkillsCard({ name, image }) {
   return (
     <>
-      <div className=" border-solid border-2 border-primary p-3 rounded-xl m-3 md:w-44 w-30 items-center text-center animate-pulse  ">
+      <div className=" border-solid border-2 border-primary p-3 rounded-xl m-3 md:w-44 w-28 items-center text-center animate-pulse  ">
         <img src={image} alt={name} />
         <p>{name}</p>
       </div>
