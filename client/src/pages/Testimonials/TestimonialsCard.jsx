d61c97cc854e82f1c8e0b876b0f1f76509258ef0
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-01 16:59:55 -0500

    nuevas actualizaciones

--- client/src/pages/Testimonials/TestimonialsCard.jsx
+++ client/src/pages/Testimonials/TestimonialsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 function TestimonialsCard({
   name,
   position,
@@ -10,14 +11,22 @@ function TestimonialsCard({
 }) {
   return (
     <>
-      <div className="flex flex-col bg-cards   rounded-xl lg:w-[450px] w-[350px] p-6 items-center m-3 ">
-        <img
-          className="w-20 h-20 rounded-full items-center"
-          src={image}
-          alt="image"
-        />
-        <p className="mb-6 mt-5 text-justify px-2 text-colortext">{comment}</p>
-        <p className="lg:text-3xl text-[20px]">{name}</p>
+      <div className="flex flex-col bg-cards   rounded-xl lg:w-[450px] w-[350px] p-6 m-3 ">
+        <div className="flex justify-center">
+          <img className="w-20 h-20 rounded-full" src={image} alt="image" />
+        </div>
+        <div className="flex  mt-2 justify-start">
+          {" "}
+          <RiDoubleQuotesL className="flex text-primary" />
+        </div>
+
+        <p className="mb-6 mt-5 text-justify px-2 text-colortext"> {comment}</p>
+        <div className="flex justify-end">
+          {" "}
+          <RiDoubleQuotesR className="text-primary" />
+        </div>
+
+        <p className="lg:text-3xl text-[20px] text-center">{name}</p>
         <div className="flex flex-row justify-center">
           <p className="lg:text-2xl mr-4 text-colortext text-[15px]">
             {position}
