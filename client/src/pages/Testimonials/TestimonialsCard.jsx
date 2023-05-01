d0901193e582abfeeecb05d438392f8183bf33ea
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-05-01 16:32:02 -0500

    actualizacion testimonials

--- client/src/pages/Testimonials/TestimonialsCard.jsx
+++ client/src/pages/Testimonials/TestimonialsCard.jsx
@@ -17,19 +17,21 @@ function TestimonialsCard({
           alt="image"
         />
         <p className="mb-6 mt-5 text-justify px-2 text-colortext">{comment}</p>
-        <p className="text-3xl">{name}</p>
+        <p className="lg:text-3xl text-[20px]">{name}</p>
         <div className="flex flex-row justify-center">
-          <p className="text-2xl mr-4">{position}</p>
+          <p className="lg:text-2xl mr-4 text-colortext text-[15px]">
+            {position}
+          </p>
 
           <a
             href={linkedin}
-            className="mt-2 mr-2 text-[20px] text-primary hover:text-secondary"
+            className="lg:mt-2 mr-2 lg:text-[20px] text-primary hover:text-secondary mt-1"
           >
             <FaLinkedin />
           </a>
           <a
             href={github}
-            className="mt-2 text-[20px] text-primary hover:text-secondary"
+            className="lg:mt-2 lg:text-[20px] text-primary hover:text-secondary mt-1"
           >
             <FaGithub />
           </a>
